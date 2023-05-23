fd74c582303c1e6c644d0e25a05d1557743fe3f3
Jordan Williams <jordan.williams@codemith.io>
2023-05-23 02:23:01 -0400

    login checks both username and password

--- helpers/loginHelpers.ts
+++ helpers/loginHelpers.ts
@@ -1,16 +1,17 @@
 import watermelonDatabase from '../watermelon/getWatermelonDb';
 import {Q} from '@nozbe/watermelondb';
 
-export const checkExistingUser = async (email: string) => {
+export const checkExistingUser = async (email: string, password: string) => {
   try {
     const existingUser = await watermelonDatabase
       .get('users')
-      .query(Q.where('email', email))
+      .query(Q.where('email', email), Q.where('password', password))
       .fetch();
     if (existingUser.length > 0) {
       await watermelonDatabase.localStorage.set('user_id', existingUser[0].id);
       await watermelonDatabase.localStorage.set(
         'username',
+        //@ts-ignore
         existingUser[0].username
       );
       return existingUser[0];
@@ -20,26 +21,31 @@ export const checkExistingUser = async (email: string) => {
   }
 };
 
-export const handleLogin = async (
-  email: string,
-  password: string,
-  setUserCb: React.Dispatch<React.SetStateAction<any>>,
-  errorCb: React.Dispatch<React.SetStateAction<any>>
-): Promise<void> => {
+export const handleLogin = async ({
+  email,
+  password,
+  setUser,
+  setError,
+}: {
+  email: string;
+  password: string;
+  setUser: React.Dispatch<React.SetStateAction<any>>;
+  setError: React.Dispatch<React.SetStateAction<any>>;
+}): Promise<void> => {
   try {
     if (email.trim() === '' || password.trim() === '') {
-      errorCb('All fields are required');
+      setError('All fields are required');
       return;
     }
     //check for exitsing user
-    const existingUser = await checkExistingUser(email);
+    const existingUser = await checkExistingUser(email, password);
 
     if (!existingUser) {
-      errorCb('Invalid Email or Password');
+      setError('Invalid Email or Password');
       return;
     }
     //set user info in app state
-    setUserCb({userId: existingUser._raw.id});
+    setUser({userId: existingUser._raw.id});
   } catch (err) {
     console.error('Error in handling Login', err);
   }
