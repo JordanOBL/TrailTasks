19edcf41cd40adca3473ab26eebf612133a7ab46
Jordan Williams <jordan.williams@codemith.io>
2023-05-26 11:39:45 -0400

    update/start new trail on start now click in Explore screen working

--- Screens/ExploreScreen.tsx
+++ Screens/ExploreScreen.tsx
@@ -1,16 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {ScrollView, StyleSheet, Text, SafeAreaView} from 'react-native';
 import NearbyTrails from '../components/NearbyTrails';
-import React from 'react'
+import React from 'react';
+import {useDatabase} from '@nozbe/watermelondb/hooks';
+import getTrails from '../helpers/Trails/getTrails';
 
 const ExploreScreen = () => {
+  const watermelonDatabase = useDatabase();
+  const {trails, setTrails} = getTrails(watermelonDatabase);
+
+  const filterTrails = (title: string, trailsArr: any): any => {
+    if (title === 'Short Trails') {
+      return trailsArr.filter(
+        (trail: any) => trail.trail_difficulty === 'short'
+      );
+    } else if (title === 'Moderate Trails') {
+      return trailsArr.filter(
+        (trail: any) => trail.trail_difficulty === 'moderate'
+      );
+    } else if (title === 'Long Trails') {
+      return trailsArr.filter((trail: any) => trail.trail_difficulty === 'long');
+    } else return trails;
+  };
+
   return (
-    <View>
-      <Text>ExploreScreen</Text>
-      <NearbyTrails sectionTitle={'Latest Trails'} />
-    </View>
-  )
-}
+    <SafeAreaView>
+      {/* <Text style={{color: 'white', padding: 10, fontSize: 18, fontWeight: 'bold'}}>FIND / ADD NEW TRAILS</Text> */}
+      {trails ? (
+        <ScrollView>
+          <NearbyTrails
+            trails={filterTrails('all trails', [...trails])}
+            sectionTitle={'Latest Trails'}
+          />
+          <NearbyTrails
+            trails={filterTrails('Short Trails', [...trails])}
+            sectionTitle={'Short Trails'}
+          />
+          <NearbyTrails
+            trails={filterTrails('Moderate Trails', [...trails])}
+            sectionTitle={'Moderate Trails'}
+          />
+          <NearbyTrails
+            trails={filterTrails('Long Trails', [...trails])}
+            sectionTitle={'Long Trails'}
+          />
+        </ScrollView>
+      ) : (
+        <Text>Loading...</Text>
+      )}
+    </SafeAreaView>
+  );
+};
 
-export default ExploreScreen
+export default ExploreScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
