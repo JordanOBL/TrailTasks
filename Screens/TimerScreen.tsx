c3bcc436119196ac9c18ecac4de25bab3c89e2d4
Jordan Williams <jordan.williams@codemith.io>
2023-06-03 19:35:23 -0400

    getbettertime function adn get timedifference for user completeing a hike, change trail progress update to a string, add fastafboi as cheat code for 26mph hiking, remove console logs, add get latest user trail to timerscreen in stead of custom hook

--- Screens/TimerScreen.tsx
+++ Screens/TimerScreen.tsx
@@ -6,17 +6,48 @@ import {UserContext} from '../App';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {useDatabase} from '@nozbe/watermelondb/hooks';
 import getJoinedUserTrail from '../helpers/Timer/getJoinedUserTrail';
-import {SessionDetails} from '../types/session';
+import {JoinedUserTrail, SessionDetails} from '../types/session';
+import {useFocusEffect} from '@react-navigation/native';
+import {save} from '../helpers/Timer/timerFlow';
+import {Q} from '@nozbe/watermelondb';
 
 const TimerScreen = () =>
 {
   //@ts-ignore
-  const {userId} = React.useContext(UserContext);
+  const { userId } = React.useContext(UserContext);
   const watermelonDatabase = useDatabase();
-  const {joinedUserTrail, setJoinedUserTrail} = getJoinedUserTrail(
+  const { joinedUserTrail, setJoinedUserTrail } = getJoinedUserTrail(
     watermelonDatabase,
     userId
   );
+  async function getLatestUserTrail()
+  {
+    try
+    {
+      const latestUserTrail = await watermelonDatabase
+        .get('users')
+        .query(
+          Q.unsafeSqlQuery(
+            'SELECT users.*, trails.*, parks.park_name, parks.park_type ' +
+            'FROM users ' +
+            'LEFT JOIN trails ON trails.id = users.trail_id  ' +
+            'LEFT JOIN parks on trails.park_id = parks.id ' +
+            'WHERE users.id = ? ',
+            [userId]
+          )
+        )
+        .unsafeFetchRaw();
+      if (latestUserTrail?.length > 0)
+      {
+        console.log({ latestUserTrail });
+        return latestUserTrail[0];
+        //await sync(watermelonDatabase);
+      }
+    } catch (err)
+    {
+      console.log('error in getloggeduser function in timerScreen', err);
+    }
+  }
   const [sessionDetails, setSessionDetails] = React.useState<SessionDetails>({
     isSessionStarted: false,
     isPaused: false,
@@ -36,12 +67,102 @@ const TimerScreen = () =>
     strikes: 0,
     endSessionModal: false,
     totalSessionTime: 0,
-    presaveDistanceHiked: 0.00,
-    totalDistanceHiked: 0.00,
+    presaveDistanceHiked: 0.0,
+    totalDistanceHiked: 0.0,
     isLoading: false,
     isError: false,
   });
 
+  // const asyncSave = async() =>
+  // {
+  //   await save({
+  //     watermelonDatabase,
+  //     userId,
+  //     setSessionDetails,
+  //     sessionDetails,
+  //     //@ts-ignore
+  //     setJoinedUserTrail,
+  //     //@ts-ignore
+  //     joinedUserTrail,
+  //     paused: true,
+  //   });
+  // }
+
+  async function compareUserChanges()
+  {
+    try
+    {
+      
+      const latestUserInfo = await getLatestUserTrail();
+      setJoinedUserTrail((prev: JoinedUserTrail) =>
+      {
+        return {
+          ...prev,
+          park_id: latestUserInfo.park_id,
+          park_name: latestUserInfo.park_name,
+          park_type: latestUserInfo.park_type,
+          trail_difficulty: latestUserInfo.trail_difficulty,
+          trail_distance: latestUserInfo.trail_distance,
+          trail_elevation: latestUserInfo.trail_elevation,
+          trail_id: latestUserInfo.trail_id,
+          trail_image_url: latestUserInfo.trail_image_url,
+          trail_lat: latestUserInfo.trail_lat,
+          trail_long: latestUserInfo.trail_long,
+          trail_name: latestUserInfo.trail_name,
+          trail_progress: latestUserInfo.trail_progress,
+          trail_started_at: latestUserInfo.trail_started,
+        };
+      });
+      await save({
+        watermelonDatabase,
+        userId,
+        setJoinedUserTrail,
+        joinedUserTrail,
+        setSessionDetails,
+        sessionDetails,
+        paused: true,
+      });
+     
+    } catch (error)
+  {
+    console.error('error in compare user changes timerscreen', error);
+  }
+}
+
+  useFocusEffect(
+    React.useCallback(() => {
+      compareUserChanges(); 
+
+      return async () =>
+      {
+        await await save({
+          watermelonDatabase,
+          userId,
+          setJoinedUserTrail,
+          joinedUserTrail,
+          setSessionDetails,
+          sessionDetails,
+          paused: true,
+        });
+        if (
+          sessionDetails.elapsedPomodoroTime <
+          sessionDetails.initialPomodoroTime
+        ) {
+          console.log({sessionDetails});
+          setSessionDetails((prev) => {
+            return {...prev, isPaused: true, strikes: prev.strikes + 1};
+          });
+        } else {
+          console.log(sessionDetails);
+          setSessionDetails((prev) => {
+            return {...prev, isPaused: true};
+          });
+        }
+
+        console.log('Timer Screen was unfocused');
+      };
+    }, [watermelonDatabase])
+  );
   return (
     <SafeAreaView style={styles.container}>
       {sessionDetails.isLoading ? (
@@ -55,7 +176,9 @@ const TimerScreen = () =>
         <SessionTimer
           sessionDetails={sessionDetails}
           setSessionDetails={setSessionDetails}
+          //@ts-ignore
           joinedUserTrail={joinedUserTrail}
+          //@ts-ignore
           setJoinedUserTrail={setJoinedUserTrail}
         />
       )}
