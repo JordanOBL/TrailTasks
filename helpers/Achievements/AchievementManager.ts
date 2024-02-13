efb3e022568a97642549adbaac74f000831ac956
JordanOBL <89749913+JordanOBL@users.noreply.github.com>
2024-02-12 22:13:03 -0500

    start achievement manager object

--- helpers/Achievements/AchievementManager.ts
+++ helpers/Achievements/AchievementManager.ts
@@ -1,4 +1,24 @@
-async function AchievementManager(user, achievements, userTotalMiles, userAchievements, userCompletedTrails)
+const AchievmentManager = 
 {
-  
+  //*This function adds a row to the the users_achievmenets database, unlocking a user Achievment
+  async unlockAchievement('')
+  //Create a function that checks for achievments about a users total miles
+  async check_total_miles_achievements()
+  {
+    try
+    {
+      //filter only achievements the user has NOT completed
+      // for each achievement in new filtered lockedAchievements Array
+        //if users total miles > achievement.condition
+          // call unlockAchievementFunction
+    } catch (err)
+    {
+      console.log('Error in check_total_milles_achievements', err)
+    }
+
+  }
+  //Create a function that checks for achievements about a users completed hikes
+  //create a function that checks for achievements about user sessions
+  //Create a function that unlocks a specific achievement
+
 }
