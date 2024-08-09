73f79b51b349b6f65581691c24dc4678131906f5
jordanobl <jordan.williams@codemith.io>
2024-08-09 17:42:37 -0400

    $(git log -1 --pretty=%B) - add server ip info for railway requests

--- helpers/loginHelpers.ts
+++ helpers/loginHelpers.ts
@@ -3,6 +3,7 @@ import {User, Subscription, User_Miles,User_Purched_Trail, User_Session, User_Ac
 import checkInternetConnection from '../helpers/InternetConnection/checkInternetConnection';
 import sync from '../watermelon/sync';
 import {prepareCreate} from '@nozbe/watermelondb/QueryDescription';
+const railwayServer = 'expressjs-postgres-production-54e4.up.railway.app'
 
 //*this function checks for a user in the database matching users login input
 export const checkExistingUser = async (
@@ -28,13 +29,17 @@ export const checkExistingGlobalUser = async (
   password: string,
 ): Promise<User | null> => {
   try {
-    const response = await fetch('http://192.168.1.42:5500/api/users', {
+    const response = await fetch(`https://${railwayServer}/api/users`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ email, password }),
     });
+
+    if(response.status == 404) {
+      return null;
+    }
 
     if (!response.ok) {
       throw new Error('Network response was not ok');
@@ -130,51 +135,6 @@ export const checkForLoggedInUser = async (
   }
 };
 
-//export const handleLogin = async ({
-//  email,
-//  password,
-//  setUser,
-//  setError,
-//  watermelonDatabase,
-//}: {
-//  email: string;
-//  password: string;
-//  setUser: React.Dispatch<React.SetStateAction<any>>;
-//  setError: React.Dispatch<React.SetStateAction<any>>;
-//  watermelonDatabase: Database;
-//}): Promise<void> => {
-//  try {
-//    if (email.trim() === '' || password.trim() === '') {
-//      setError('All fields are required');
-//      return;
-//    }
-//    //check for exitsing user
-//    const existingUser = await checkExistingUser(
-//      email,
-//      password,
-//      watermelonDatabase
-//    );
-//
-//    if (!existingUser) {
-//      setError('Invalid Email or Password');
-//      return;
-//    }
-//    //@ts-ignore
-//    await setSubscriptionStatus(existingUser, watermelonDatabase);
-//    const response = await setLocalStorageUserAndMiles(
-//      existingUser,
-//      watermelonDatabase
-//    );
-//    if (response) {
-//      setUser(existingUser);
-//      return;
-//    }
-//  } catch (err) {
-//    console.error('Error in handling Login', err);
-//  }
-//};
-
-
 
 export const handleLogin = async ({
   email,
@@ -203,10 +163,9 @@ export const handleLogin = async ({
     // If not found locally and internet is available, check globally
     if (!existingUser && connection.isConnected) {
       existingUser = await checkExistingGlobalUser(email, password);
-      console.log('Global existingUser:', existingUser);
 
       if (existingUser) {
-      console.debug('existingUser found from checkExistingGlobalUser',{existingUser})
+       console.log('existingUser subscription', existingUser.userSubscription)
         // Save the user and related data to local database
         await watermelonDatabase.write(async () => {
             // Create user
@@ -226,14 +185,12 @@ export const handleLogin = async ({
               newUser.trailStartedAt = existingUser.user.trail_started_at;
               newUser.trailTokens = existingUser.user.trail_tokens;
             })
-          console.debug({newUser})
             // Create user miles
             const userMiles = watermelonDatabase.collections.get('users_miles').prepareCreate((newUserMiles: User_Miles) => {
               newUserMiles._raw.id = existingUser.userMiles.id;
               newUserMiles.totalMiles = existingUser.userMiles.total_miles;
               newUserMiles.userId = existingUser.userMiles.user_id;
        })
-            console.debug({userMiles})
 
             // Create user sessions
             const userSessions = [...existingUser.userSessions].map((session: User_Session) =>
@@ -251,10 +208,9 @@ export const handleLogin = async ({
                 newUserSession.sessionEndedAt = session.session_ended_at;
               })
             )
-          console.debug({userSessions})
 //            // Create user purchased trail
             const userPurchasedTrails = [...existingUser.userPurchasedTrails].map((trail: User_Purchased_Trail) =>
-              watermelonDatabase.collections.get('users_purchased_trails').prepareCreate((newUserPurchasedTrail: UserPurchasedTrail) => {
+              watermelonDatabase.collections.get('users_purchased_trails').prepareCreate((newUserPurchasedTrail: User_Purchased_Trail) => {
                 newUserPurchasedTrail._raw.id = trail.id;
                 newUserPurchasedTrail.userId = trail.user_id;
                 newUserPurchasedTrail.purchasedAt = trail.purchased_at;
@@ -262,15 +218,15 @@ export const handleLogin = async ({
                 newUserPurchasedTrail.createdAt = trail.created_at;
               })
             )  
-            console.debug({userPurchasedTrails})
 //            // Create user subscription
             const userSubscriptions =  watermelonDatabase.collections.get('users_subscriptions').prepareCreate((newUserSubscription: Subscription) => {
               newUserSubscription._raw.id = existingUser.userSubscription.id;
               newUserSubscription.userId = existingUser.userSubscription.user_id;
               newUserSubscription.isActive = existingUser.userSubscription.is_active;
+              newUserSubscription.expiresAt = existingUser.userSubscription.expires_at;
             })
 //            // Create user achievements
-            const userAchievements = [...existingUser.userAchievements].map((achievement: UserAchievement) =>
+            const userAchievements = [...existingUser.userAchievements].map((achievement: User_Achievement) =>
               watermelonDatabase.collections.get('users_achievements').prepareCreate((newUserAchievement: User_Achievement) => {
                 newUserAchievement._raw.id = achievement.id;
                 newUserAchievement.userId = achievement.user_id;
@@ -286,7 +242,7 @@ export const handleLogin = async ({
 
         // Retrieve the newly created user from the database
         existingUser = await checkExistingUser(email, password, watermelonDatabase);
-        await sync(watermelonDatabase, existingUser.id);
+        //await sync(watermelonDatabase, existingUser.id);
       }
     }
 
