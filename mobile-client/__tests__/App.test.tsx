061a3f395478bb4d03b30a9d26e965bdb7abc25a
jordan williams <jordanwilliams.dev@gmail.com>
2026-08-16 21:58:01 -0400

    remove jest import

--- mobile-client/__tests__/App.test.tsx
+++ mobile-client/__tests__/App.test.tsx
@@ -1,16 +1,18 @@
 // @ts-nocheck
 import { fireEvent, render, waitFor } from "@testing-library/react-native";
-import test, { describe } from "node:test";
 
 import { WrappedApp } from "../index";
 import { testDb as watermelonDatabase } from "../watermelon/testDB";
 
-describe("App", () => {
-  test("loads initial bootsrap trails on first app load", async () => {
+describe("App", () =>
+{
+  test("loads initial bootsrap trails on first app load", async () =>
+  {
     const { getByTestId } = render(<WrappedApp />);
     // Wait up to 10 seconds for a condition
     await waitFor(
-      async () => {
+      async () =>
+      {
         const trails = await watermelonDatabase.get("trails").query().fetch();
         // Some assertion that ensures bootstrap is done
         expect(trails.length).toBe(96);
